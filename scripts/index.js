a70c9223ef80e43aad7cf494951243288c000acd
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-03 03:33:38 +0200

    creation du index.js afin de recupéré le data et de créer tous les card de menu pour qu'il s'affiche au lancement de la page

---
+++ scripts/index.js
@@ -0,0 +1,67 @@
+let recipes = [];
+
+async function getData() {
+  try {
+    const response = await fetch("/data/recipes.json");
+    const data = await response.json();
+    recipes = data.recipes;
+    console.log(recipes);
+    return recipes;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const CreatCard = (recipe) => {
+  const article = document.querySelector(".card");
+
+  const imgCard = document.createElement("div");
+  imgCard.classList.add("imgCard");
+  article.appendChild(imgCard);
+
+  const txtCard = document.createElement("div");
+  txtCard.classList.add("txtArticle");
+  article.appendChild(txtCard);
+
+  const headerCard = document.createElement("div");
+  headerCard.classList.add("d-flex", "justify-content-between", "mb-3");
+  txtCard.appendChild(headerCard);
+
+  const nameMenu = document.createElement("div");
+  nameMenu.classList.add("nomMenu");
+  nameMenu.textContent = recipe.name;
+  headerCard.appendChild(nameMenu);
+
+  const time = document.createElement("div");
+  time.classList.add("nomMenu");
+  headerCard.appendChild(time);
+
+  const svgTime = document.createElement("img");
+  const svg = "../Picture/time.svg";
+  svgTime.setAttribute("src", svg);
+  svgTime.classList.add("svgTime");
+  time.appendChild(svgTime);
+
+  const timePreparation = document.createElement("span");
+  timePreparation.classList.add("timePreparation");
+  timePreparation.textContent = recipe.time
+  time.appendChild(timePreparation);
+};
+
+async function init() {
+  try {
+    const response = await fetch("/data/recipes.json");
+    const data = await response.json();
+    const recipes = data.recipes;
+    
+    for (const recipe of recipes) {
+      CreatCard(recipe);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
+
+init();
