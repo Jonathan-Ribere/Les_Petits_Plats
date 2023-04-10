dbeaae2151408e22ca4b9d73f3481cfc9236e7b0
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-10 19:37:57 +0200

    Mise en place de la condition des 3 caractères : si l'utilisateur a entrer moin de 3 caractères il affiche dans la consol : Veuillez entrer au moins 3 caractères pour lancer la recherche et si plus de 3 caractères alors il affiche dans la console les caractères taper

--- scripts/tri.js
+++ scripts/tri.js
@@ -1,4 +1,9 @@
 let searchInput = document.querySelector('#search-input');
 searchInput.addEventListener('input', function(event) {
-    console.log('Recherche en cours...');
+    let caractere = event.target.value; 
+    if (caractere.length >= 3) {
+      console.log('Recherche en cours pour : ' + caractere);
+    } else {
+      console.log('Veuillez entrer au moins 3 caractères pour lancer la recherche');
+    }
 });
