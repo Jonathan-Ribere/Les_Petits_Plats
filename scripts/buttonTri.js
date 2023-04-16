ec8429e7a92735dd924b6ed99a9dee332f109891
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-16 16:23:42 +0200

    mise en place de la partie pour afficher les li dans le button ustensiles

--- scripts/buttonTri.js
+++ scripts/buttonTri.js
@@ -1,1 +1,39 @@
-console.log("buttonTri");
+// Récupération de l'élément bouton et de la liste
+const buttonUstensiles = document.getElementById("buttonUstensiles");
+const listUstensiles = document.getElementById("listUstensiles");
+
+// Création d'un tableau vide pour stocker les données du JSON
+let recipes = [];
+
+async function getDataUstensiles() {
+  try {
+    const response = await fetch("/data/recipes.json");
+    const data = await response.json();
+    recipes = data.recipes;
+    return recipes;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function init() {
+  // Récupérer les données de JSON
+  await getDataUstensiles();
+
+  // Créer un tableau de tous les ustensiles uniques
+  const allUstensiles = new Set();
+  recipes.forEach((recipe) => {
+    recipe.ustensils.forEach((ustensil) => {
+      allUstensiles.add(ustensil.toLowerCase()); // Ajouter en minuscule pour éviter les doublons
+    });
+  });
+
+  // Créer les éléments de la liste et les ajouter à l'élément de liste HTML
+  allUstensiles.forEach((ustensil) => {
+    const li = document.createElement("li");
+    li.textContent = ustensil;
+    listUstensiles.appendChild(li);
+  });
+}
+
+init();
