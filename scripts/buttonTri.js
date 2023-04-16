e4da237a086494fab38e538347a7e93e613a3cf0
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-16 17:40:00 +0200

    creation de la function pour le button appareils

--- scripts/buttonTri.js
+++ scripts/buttonTri.js
@@ -14,34 +14,47 @@ const listUstensiles = document.getElementById("listUstensiles");
 let recipes = [];
 
 const getData = async () => {
-    try {
-        const response = await fetch("/data/recipes.json");
-        const data = await response.json();
-        recipes = data.recipes;
-        return recipes;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
+  try {
+    const response = await fetch("/data/recipes.json");
+    const data = await response.json();
+    recipes = data.recipes;
+    return recipes;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const displayListeIngredients = () => {
-  
-   // Créer un tableau de tous les Ingredients uniques
-   const allIngredients = new Set();
-   recipes.forEach((recipe) => {
-     recipe.ingredients.forEach((ingredient) => {
-       allIngredients.add(ingredient.ingredient.toLowerCase()); // Ajouter en minuscule pour éviter les doublons
-     });
-   });
- 
-   // Créer les éléments de la liste et les ajouter à l'élément de liste HTML
-   allIngredients.forEach((ingredient) => {
-     const li = document.createElement("li");
-     li.textContent = ingredient;
-     listIngredients.appendChild(li);
-   });
-  };
+  // Créer un tableau de tous les Ingredients uniques
+  const allIngredients = new Set();
+  recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((ingredient) => {
+      allIngredients.add(ingredient.ingredient.toLowerCase()); // Ajouter en minuscule pour éviter les doublons
+    });
+  });
+
+  // Créer les éléments de la liste et les ajouter à l'élément de liste HTML
+  allIngredients.forEach((ingredient) => {
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    listIngredients.appendChild(li);
+  });
+};
+
+const displayListeAppareils = () => {
+  // Créer un tableau de tous les Appareils uniques
+  const allAppareils = new Set();
+  recipes.forEach((recipe) => {
+    allAppareils.add(recipe.appliance.toLowerCase()); // Ajouter en minuscule pour éviter les doublons
+  });
+
+  // Créer les éléments de la liste et les ajouter à l'élément de liste HTML
+  allAppareils.forEach((appareil) => {
+    const li = document.createElement("li");
+    li.textContent = appareil;
+    listAppareils.appendChild(li);
+  });
+};
 
 const displayListeUstensiles = () => {
   // Créer un tableau de tous les ustensiles uniques
@@ -58,12 +71,13 @@ const displayListeUstensiles = () => {
     li.textContent = ustensil;
     listUstensiles.appendChild(li);
   });
-}
+};
 
 const init = async () => {
   await getData();
-  displayListeIngredients()
+  displayListeIngredients();
+  displayListeAppareils();
   displayListeUstensiles();
-}
+};
 
 init();
