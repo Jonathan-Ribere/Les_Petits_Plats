468409b664de4976f78e32082fd7f55002bdb78a
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-16 15:17:17 +0200

    corection du bug quand j'effacée les caratères

--- scripts/tri.js
+++ scripts/tri.js
@@ -1,12 +1,16 @@
 let searchInput = document.querySelector("#search-input");
+
 searchInput.addEventListener("input", function (event) {
   let caractere = event.target.value.toLowerCase();
+
   if (caractere.length >= 3) {
     console.log("Recherche en cours pour : " + caractere);
     let items = document.querySelectorAll(".article");
+
     for (let i = 0; i < items.length; i++) {
       let item = items[i];
       let title = item.querySelector(".nomMenu").textContent.toLowerCase();
+      
       if (title.includes(caractere)) {
         item.style.display = "block";
       } else {
@@ -23,7 +27,7 @@ searchInput.addEventListener("input", function (event) {
 });
 
 function resetSearch() {
-    let items = document.querySelectorAll('.card');
+    let items = document.querySelectorAll('.article');
     for (let i = 0; i < items.length; i++) {
       let item = items[i];
       item.style.display = 'block';
