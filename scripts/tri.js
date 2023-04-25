e44ea303c2c98064a1528f163eead3445442305a
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-25 13:19:46 +0200

    mis en place du foreach pour les ingredient

--- scripts/tri.js
+++ scripts/tri.js
@@ -29,13 +29,10 @@ const sortArticles = (articles) => {
 
 // Afficher les articles qui correspondent à la recherche
 const displayArticles = (articles) => {
-  // const ingredients = articles.ingredients;
-
   articlesContainer.innerHTML = "";
   articles.forEach((article) => {
     const articleElement = document.createElement("article");
     articleElement.classList.add("article");
-
     articlesContainer.appendChild(articleElement);
 
     const imgCard = document.createElement("div");
@@ -52,9 +49,7 @@ const displayArticles = (articles) => {
 
     const nameMenu = document.createElement("div");
     nameMenu.classList.add("nomMenu");
-    nameMenu.innerHTML = `
-    <h2>${article.name}</h2>
-  `;
+    nameMenu.innerHTML = `<h2>${article.name}</h2>`;
     headerCard.appendChild(nameMenu);
 
     const time = document.createElement("div");
@@ -69,10 +64,7 @@ const displayArticles = (articles) => {
 
     const timePreparation = document.createElement("span");
     timePreparation.classList.add("timePreparation");
-    timePreparation.textContent = "min";
-    timePreparation.innerHTML = `
-    <p>${article.time}</p>
-  `;
+    timePreparation.innerHTML = `<p>${article.time}</p>`;
     time.appendChild(timePreparation);
 
     const mainCard = document.createElement("div");
@@ -87,15 +79,19 @@ const displayArticles = (articles) => {
     ul.classList.add("ulList");
     divUl.appendChild(ul);
 
+    article.ingredients.forEach((ingredient) => {
+      const li = document.createElement("li");
+      li.textContent = ingredient.ingredient + ":" + ingredient.quantity + ingredient.unit ;
+      ul.appendChild(li);
+    });
+
     const prepa = document.createElement("div");
     prepa.classList.add("prepa");
     mainCard.appendChild(prepa);
 
     const prepaRecette = document.createElement("p");
     prepaRecette.classList.add("recette");
-    prepaRecette.innerHTML = `
-    <p>${article.description}</p>
-  `;
+    prepaRecette.innerHTML = `<p>${article.description}</p>`;
     prepa.appendChild(prepaRecette);
   });
 };
@@ -108,20 +104,15 @@ searchInput.addEventListener("input", () => {
 /*
 const tableau = await getData()
 console.log(tableau);
-
 const resultArray = [];
-
-
 // Ajouter un événement qui se déclenche à chaque fois que l'utilisateur entre du texte dans le champ de recherche
 searchInput.addEventListener("input", function (event) {
   // Récupérer le texte entré dans le champ de recherche et le mettre en minuscule
   let searchValue = event.target.value.toLowerCase();
-
   // Si la longueur du texte entré est supérieure ou égale à 3 caractères
   if (searchValue.length >= 3) {
     // Afficher un message dans la console pour indiquer que la recherche est en cours
     console.log("Recherche en cours pour : " + searchValue);
-
     for (let i = 0; i < tableau.length; i++) {
       const obj = tableau[i];
       console.log(obj);
--- test.js
+++ test.js
@@ -1,19 +1,149 @@
 import { getData } from "./api.js";
 
+const searchInput = document.querySelector("#search-input");
+const articlesContainer = document.querySelector("#section");
+
+let articles = [];
+
+// Utiliser une fonction fléchée pour rendre la syntaxe plus concise
+const sortArticles = (articles) => {
+const searchValue = searchInput.value.toLowerCase();
+return articles.filter((article) => {
+// Utiliser la décomposition pour accéder aux propriétés de l'article
+const { appliance, name, description } = article;
+return (
+appliance.toLowerCase().includes(searchValue) ||
+name.toLowerCase().includes(searchValue) ||
+description.toLowerCase().includes(searchValue)
+);
+});
+};
+
+// Utiliser la syntaxe de destructuration pour rendre le code plus lisible
+const displayIngredientsList = (recipes) => {
+  // On crée un tableau qui contiendra tous les ingrédients
+  const ingredients = [];
+
+  // On parcourt chaque recette
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    console.log(recipe);
+    // On parcourt chaque ingrédient de la recette
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      const ingredient = recipe.ingredients[j];
+      // On ajoute l'ingrédient à notre tableau s'il n'y est pas déjà
+      if (!ingredients.includes(ingredient.ingredient)) {
+        ingredients.push(ingredient.ingredient);
+      }
+    }
+  }
+
+  // On crée une liste HTML avec les ingrédients
+  const ul = document.createElement("ul");
+  for (let i = 0; i < ingredients.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = ingredients[i];
+    ul.appendChild(li);
+  }
+
+  // On affiche la liste dans la page
+  return ul;
+};
+
+const displayArticles = (articles) => {
+// Utiliser innerHTML pour effacer le contenu de l'élément
+articlesContainer.innerHTML = "";
+for (const article of articles) {
+const { name, time, description, recipes } = article;
+const articleElement = document.createElement("article");
+articleElement.classList.add("article");
+articlesContainer.appendChild(articleElement);
+
+const imgCard = document.createElement("div");
+imgCard.classList.add("imgArticle");
+articleElement.appendChild(imgCard);
+
+const txtCard = document.createElement("div");
+txtCard.classList.add("txtArticle");
+articleElement.appendChild(txtCard);
+
+const headerCard = document.createElement("div");
+headerCard.classList.add("headerCard");
+txtCard.appendChild(headerCard);
+
+const nameMenu = document.createElement("div");
+nameMenu.classList.add("nomMenu");
+nameMenu.innerHTML = `<h2>${name}</h2>`;
+headerCard.appendChild(nameMenu);
+
+const timeElement = document.createElement("div");
+timeElement.classList.add("nomMenu");
+headerCard.appendChild(timeElement);
+
+const svgTime = document.createElement("img");
+const svg = "../Picture/time.svg";
+svgTime.setAttribute("src", svg);
+svgTime.classList.add("svgTime");
+timeElement.appendChild(svgTime);
+
+const timePreparation = document.createElement("span");
+timePreparation.classList.add("timePreparation");
+timePreparation.innerHTML = `<p>${time} min</p>`;
+timeElement.appendChild(timePreparation);
+
+const mainCard = document.createElement("div");
+mainCard.classList.add("mainCard");
+txtCard.appendChild(mainCard);
+
+const divUl = document.createElement("div");
+divUl.classList.add("divUl");
+mainCard.appendChild(divUl);
+
+const ul = displayIngredientsList(recipes);
+ul.classList.add("ulList");
+divUl.appendChild(ul);
+
+const prepa = document.createElement("div");
+prepa.classList.add("prepa");
+mainCard.appendChild(prepa);
+
+const prepaRecette = document.createElement("p");
+prepaRecette.classList.add("recette");
+prepaRecette.innerHTML = `<p>${description}</p>`;
+prepa.appendChild(prepaRecette);
+
+  };
+};
+
+// Utiliser async/await pour simplifier la gestion des promesses
+const fetchArticles = async () => {
+  try {
+  const data = await getData();
+  articles = data;
+  displayArticles(articles);
+  } catch (error) {
+  console.log(error);
+  }
+  };
+  
+  // Utiliser la méthode addEventListener pour détecter les événements sur l'input
+  searchInput.addEventListener("input", () => {
+  const sortedArticles = sortArticles(articles);
+  displayArticles(sortedArticles);
+  });
+
+  fetchArticles();
+
+
+
+
+
+/*
 const tableau = await getData()
 console.log(tableau);
 
 const resultArray = [];
 
-// Sélectionner l'élément input qui servira de champ de recherche
-let searchInput = document.querySelector("#search-input");
-
-// Sélectionner l'élément parent qui contient tous les articles
-let articlesContainer = document.querySelector("#section");
-console.log(articlesContainer);
-
-// Sélectionner tous les éléments qui contiennent les articles à rechercher
-let articles = articlesContainer.querySelectorAll(".article");
 
 // Ajouter un événement qui se déclenche à chaque fois que l'utilisateur entre du texte dans le champ de recherche
 searchInput.addEventListener("input", function (event) {
@@ -35,74 +165,4 @@ searchInput.addEventListener("input", function (event) {
     }
     
     console.log(resultArray); }})
-/*
-    // Sélectionner tous les éléments qui contiennent les articles à rechercher
-    let items = document.querySelectorAll(".article");
-
-    // Filtrer les articles pour ne conserver que ceux qui contiennent le texte recherché
-    let filteredArticles = articlesArray.filter(function (article) {
-      // Récupérer le titre de l'article en minuscule
-      let title = article.querySelector(".nomMenu").textContent.toLowerCase();
-
-      // Récupérer le contenu de l'article en minuscule
-      let content = article.querySelector(".recette").textContent.toLowerCase();
-
-      // Vérifier si le texte recherché est présent dans le titre ou le contenu de l'article
-      return title.includes(searchValue) || content.includes(searchValue);*/
-/*
-
-
-
-// Convertir les articles en tableau pour pouvoir utiliser la méthode filter() dessus
-let articlesArray = Array.from(articles);
-console.log(articlesArray);
-
-
-    // Vider le contenu de l'élément parent
-    articlesContainer.innerHTML = "";
-
-    // Créer une carte pour chaque article filtré et l'ajouter à l'élément parent
-    filteredArticles.forEach(function (article) {
-      CreatCard(article);
-    });
-    /*
-    // Parcourir tous les éléments contenant les articles
-    for (let i = 0; i < items.length; i++) {
-      // Récupérer l'élément actuel dans la boucle
-      let item = items[i];
-
-      // Récupérer le titre de l'article contenu dans l'élément actuel et le mettre en minuscule
-      let title = item.querySelector(".nomMenu").textContent.toLowerCase();
-
-      // Vérifier si le titre de l'article contient le texte entré dans le champ de recherche
-      if (title.includes(caractere)) {
-        // Si c'est le cas, afficher l'élément actuel
-        item.style.display = "block";
-      } else {
-        // Sinon, masquer l'élément actuel
-        item.style.display = "none";
-      }
-    } /////////////
-  } else {
-    // Si la longueur du texte entré est inférieure à 3 caractères, afficher tous les articles
-    articlesArray.forEach(function (article) {
-      CreatCard(article);
-    });
-  }
-});
-
-// Fonction pour réinitialiser la recherche et afficher tous les articles
-function resetSearch() {
-  // Sélectionner tous les éléments qui contiennent les articles à rechercher
-  let items = document.querySelectorAll(".article");
-
-  // Parcourir tous les éléments contenant les articles
-  for (let i = 0; i < items.length; i++) {
-    // Récupérer l'élément actuel dans la boucle
-    let item = items[i];
-
-    // Afficher l'élément actuel
-    item.style.display = "block";
-  }
-}
 */
