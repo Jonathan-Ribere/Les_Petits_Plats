ae28068a4e75c71d9784f9050c8fd2bb36b031ea
Jonathan-Ribiere <j_ribiere@icloud.com>
2023-04-27 17:00:08 +0200

    evitée d'avoir les doublon dans l'affichage apres le tri

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
 export const displayIngredientsList = (recipes) => {
-  const ingredients = [];
+  const ingredients = {};
   console.log("longueur", recipes.length);
-  ingredients.length = 0; // vider le tableau avant de le remplir à nouveau
   for (let i = 0; i < recipes.length; i++) {
     const recipe = recipes[i];
     for (let j = 0; j < recipe.ingredients.length; j++) {
       const ingredient = recipe.ingredients[j];
       console.log(ingredient);
-      if (!ingredients.includes(ingredient.ingredient)) {
-        ingredients.push(ingredient.ingredient);
+      if (!ingredients[ingredient.ingredient]) {
+        ingredients[ingredient.ingredient] = true;
       }
     }
   }
 
   const ul = document.createElement("ul");
-  for (let i = 0; i < ingredients.length; i++) {
+  for (let ingredient in ingredients) {
     const li = document.createElement("li");
-    li.textContent = ingredients[i];
+    li.textContent = ingredient;
     console.log("la liste", li);
     ul.appendChild(li);
   }
